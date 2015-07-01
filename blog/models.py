3d56f972fdef1bf5b067d8a2be9c01fcf768b685
TakesxiSximada <takesxi.sximada@gmail.com>
2015-07-01 22:48:57 +0900

    add publish function in article models

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
@@ -11,6 +12,9 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_at = datetime.now()
 
     def __str__(self):
         return self.subject
