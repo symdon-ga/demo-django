e385b4a7ffa9146e451c64e66e2bd37610ebbcff
TakesxiSximada <takesxi.sximada@gmail.com>
2015-07-01 22:40:52 +0900

    The options auto_now, auto_now_add, and default are mutually exclusive

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,6 @@ class Article(models.Model):
     subject = models.CharField(max_length=0xFF)
     body = models.TextField()
 
-    created_at = models.DateTimeField(auto_now=True, auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
