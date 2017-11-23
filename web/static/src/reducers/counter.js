6c15e6a1d7460cb85a7adaa763193539e54f78e2
TakesxiSximada <takesxi.sximada@gmail.com>
2017-11-23 11:23:28 +0900

    add axios api call

--- web/static/src/reducers/counter.js
+++ web/static/src/reducers/counter.js
@@ -1,6 +1,6 @@
 import { INCREMENT, DECREMENT } from '../constants/ActionTypes'
 
-// import axios from 'axios'
+import axios from 'axios'
 
 
 const initialState = {
@@ -9,15 +9,14 @@ const initialState = {
 
 
 export default (state = initialState, action) => {
-    // axios.get('/api/counter/2/increment/').then((response) => {
-    //     console.log(response)
-    // })
 
     switch(action.type) {
     case INCREMENT:
-        return {
-            value: state.value + 1,
-        }
+        return axios.get('/api/counter/2/increment/').then((response) => {
+            return {
+                value: response.data.value + 1,
+            }
+        })
     case DECREMENT:
         return {
             value: state.value - 1,
